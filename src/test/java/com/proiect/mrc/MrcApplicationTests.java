3b785a959fefbc1e1b1ab9cbdb7048a843344014
Roberto Mantale <96843044+manTTale@users.noreply.github.com>
2022-10-20 17:00:44 +0300

    initial commit for the Handyman REST API

---
+++ src/test/java/com/proiect/mrc/MrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect.mrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
